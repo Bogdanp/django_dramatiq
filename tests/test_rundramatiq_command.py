97e4022b6725e894de9d682a7677e4c372cab5f9
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-01 13:32:16 +0200

    test: fix rundramatiq test for travis

--- tests/test_rundramatiq_command.py
+++ tests/test_rundramatiq_command.py
@@ -25,8 +25,9 @@ def test_rundramatiq_can_run_dramatiq(execvp_mock):
     assert "Discovered tasks module: 'tests.testapp.tasks'" in buff.getvalue()
 
     # And execvp should be called with the appropriate arguments
+    cores = str(rundramatiq.CPU_COUNT)
     execvp_mock.assert_called_once_with("dramatiq", [
-        "dramatiq", "--processes", "8", "--threads", "8",
+        "dramatiq", "--processes", cores, "--threads", cores,
         "--watch", path_to().replace("/tests", ""),
         "django_dramatiq.setup",
         "tests.testapp.tasks",
