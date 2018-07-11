3b309303eb201d230ee22b8042e8e2d8ef735b56
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2018-07-11 11:22:21 -0300

    feat: add support for setting DRAMATIQ_ENCODER

--- django_dramatiq/apps.py
+++ django_dramatiq/apps.py
@@ -24,6 +24,7 @@ DEFAULT_SETTINGS = {
         "django_dramatiq.middleware.DbConnectionsMiddleware",
     ]
 }
+DEFAULT_ENCODER = "dramatiq.encoder.JSONEncoder"
 
 
 class DjangoDramatiqConfig(AppConfig):
@@ -31,13 +32,14 @@ class DjangoDramatiqConfig(AppConfig):
     verbose_name = "Django Dramatiq"
 
     def ready(self):
-        broker_settings = type(self).broker_settings()
+        broker_settings = self.broker_settings()
         broker_path = broker_settings["BROKER"]
         broker_class = load_class(broker_path)
         broker_options = broker_settings.get("OPTIONS", {})
         middleware = [load_middleware(path) for path in broker_settings.get("MIDDLEWARE", [])]
         broker = broker_class(middleware=middleware, **broker_options)
         dramatiq.set_broker(broker)
+        dramatiq.set_encoder(self.select_encoder())
 
     @classmethod
     def broker_settings(cls):
@@ -46,3 +48,8 @@ class DjangoDramatiqConfig(AppConfig):
     @classmethod
     def tasks_database(cls):
         return getattr(settings, "DRAMATIQ_TASKS_DATABASE", "default")
+
+    @classmethod
+    def select_encoder(cls):
+        encoder = getattr(settings, "DRAMATIQ_ENCODER", DEFAULT_ENCODER)
+        return load_class(encoder)()
