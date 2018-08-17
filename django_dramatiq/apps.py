59d102f237e779f020bb8a2b42b92ac112c781d0
Dmitry Odzerikho <dmitry.odzerikho@gmail.com>
2018-08-17 15:25:23 +0800

    configurable results backend via settings

--- django_dramatiq/apps.py
+++ django_dramatiq/apps.py
@@ -1,7 +1,7 @@
 import dramatiq
 from django.apps import AppConfig
 from django.conf import settings
-
+from dramatiq.results import Results
 from .utils import load_class, load_middleware
 
 DEFAULT_BROKER = "dramatiq.brokers.rabbitmq.RabbitmqBroker"
@@ -37,8 +37,23 @@ class DjangoDramatiqConfig(AppConfig):
         broker_options = broker_settings.get("OPTIONS", {})
         middleware = [load_middleware(path) for path in broker_settings.get("MIDDLEWARE", [])]
         broker = broker_class(middleware=middleware, **broker_options)
+        self.load_results_backend(broker, broker_settings)
         dramatiq.set_broker(broker)
         dramatiq.set_encoder(self.select_encoder())
+
+    @classmethod
+    def load_results_backend(self, broker, broker_settings):
+        backend_settings = broker_settings.get("RESULTS_BACKEND", "dramatiq.results.backends.StubBackend")
+        backend_class = load_class(backend_settings)
+        backend_options = broker_settings.get("RESULTS_BACKEND_OPTIONS", {})
+        backend = backend_class(**backend_options)
+        for middleware in broker.middleware:
+            if isinstance(middleware, Results):
+                middleware.backend = backend
+                break
+        else:
+            result_middleware = load_class("dramatiq.results.middleware.Results")(backend)
+            broker.add_middleware(result_middleware)
 
     @classmethod
     def broker_settings(cls):
