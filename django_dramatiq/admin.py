a011948e99cfe2686579f39236ea92667341c8b5
jcass77 <john.cass77@gmail.com>
2019-08-16 12:05:57 +0200

    admin: switch to interrogating data for filter choices.
    Add created_at filter.

--- django_dramatiq/admin.py
+++ django_dramatiq/admin.py
@@ -2,33 +2,38 @@ import abc
 import json
 from datetime import datetime
 
-import dramatiq
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
 from django.utils import timezone
 from django.utils.safestring import mark_safe
+from dramatiq import Message
 
 from .models import Task
 
 
 class TaskFilter(abc.ABC, SimpleListFilter):
-
-    @property
-    def declared_broker_objects(self):
-        broker = dramatiq.get_broker()
-        return {
-            "actor_name": broker.get_declared_actors(),
-            "queue_name": broker.get_declared_queues().union(broker.get_declared_delay_queues())
-        }
-
     def lookups(self, request, model_admin):
-        lookup_choices = self.declared_broker_objects[self.parameter_name]
-        return [(name, "{} (slow)".format(name)) for name in lookup_choices]
-    
+        lookup_choices = set()
+
+        messages = (
+            Message.decode(bytes(encoded_message))
+            for encoded_message in model_admin.model.tasks.values_list(
+                "message_data", flat=True
+            )
+        )
+
+        for message in messages:
+            choice = getattr(message, self.parameter_name)
+            lookup_choices.add((choice, "{} (slow)".format(choice)))
+
+        return sorted(lookup_choices)
+
     def queryset(self, request, queryset):
         if self.value():
             filter_ids = (
-                str(task.id) for task in queryset if getattr(task.message, self.parameter_name) == self.value()
+                str(task.id)
+                for task in queryset
+                if getattr(task.message, self.parameter_name) == self.value()
             )
             return queryset.filter(id__in=filter_ids)
 
@@ -49,8 +54,16 @@ class ActorNameFilter(TaskFilter):
 class TaskAdmin(admin.ModelAdmin):
     exclude = ("message_data",)
     readonly_fields = ("message_details", "traceback", "status")
-    list_display = ("__str__", "status", "eta", "created_at", "updated_at", "queue_name", "actor_name")
-    list_filter = ("status", QueueNameFilter, ActorNameFilter)
+    list_display = (
+        "__str__",
+        "status",
+        "eta",
+        "created_at",
+        "updated_at",
+        "queue_name",
+        "actor_name",
+    )
+    list_filter = ("status", "created_at", QueueNameFilter, ActorNameFilter)
 
     def queue_name(self, instance):
         return instance.message.queue_name
@@ -59,7 +72,10 @@ class TaskAdmin(admin.ModelAdmin):
         return instance.message.actor_name
 
     def eta(self, instance):
-        timestamp = instance.message.options.get("eta", instance.message.message_timestamp) / 1000
+        timestamp = (
+            instance.message.options.get("eta", instance.message.message_timestamp)
+            / 1_000
+        )
         return timezone.make_aware(datetime.utcfromtimestamp(timestamp))
 
     def message_details(self, instance):
