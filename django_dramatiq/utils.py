e4ec440095d8ef1a28689868ad7cee5608632f6c
Curtis Maloney <curtis@tinbrain.net>
2020-02-24 10:50:43 +1100

    Use django's existing import-from-string util

--- django_dramatiq/utils.py
+++ django_dramatiq/utils.py
@@ -1,18 +1,7 @@
-import importlib
-
-
-def load_class(path):
-    try:
-        module_path, _, class_name = path.rpartition(".")
-        module = importlib.import_module(module_path)
-        return getattr(module, class_name)
-    except AttributeError:
-        raise ImportError("Module '%s' doesn't have a class named '%s'." % (
-            module_path, class_name,
-        ))
+from django.utils.module_loading import import_string
 
 
 def load_middleware(path_or_obj):
     if isinstance(path_or_obj, str):
-        return load_class(path_or_obj)()
+        return import_string(path_or_obj)()
     return path_or_obj
