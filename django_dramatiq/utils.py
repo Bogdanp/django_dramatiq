a4b3ab867059e4f03789727dd64ba7096d316022
André Cimander <a.cimander@protonmail.com>
2020-04-10 21:01:51 +0200

    restored accidentally deleted function.

--- django_dramatiq/utils.py
+++ django_dramatiq/utils.py
@@ -1,5 +1,12 @@
 import dramatiq
 from django.conf import settings
+from django.utils.module_loading import import_string
+
+
+def load_middleware(path_or_obj):
+    if isinstance(path_or_obj, str):
+        return import_string(path_or_obj)()
+    return path_or_obj
 
 
 def actor(fn=None, **kwargs):
