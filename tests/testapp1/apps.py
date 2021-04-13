a6d7045a463ca5edb5097b01c6cd3b5a62c84240
Ashley Bartlett <abartlett@atlassian.com>
2021-04-13 17:05:49 +1000

    fix: Name import for django 3.2 compatibility

--- tests/testapp1/apps.py
+++ tests/testapp1/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class Testapp1Config(AppConfig):
-    name = "testapp1"
+    name = "tests.testapp1"
--- tests/testapp2/apps.py
+++ tests/testapp2/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class Testapp2Config(AppConfig):
-    name = "testapp2"
+    name = "tests.testapp2"
