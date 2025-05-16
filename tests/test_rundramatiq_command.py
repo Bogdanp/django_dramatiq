a7212745249042d9e6b335b4c4fa2ce5b9b3d195
Daniel Bowring <daniel@spaceduck.com>
2025-05-16 16:40:51 +1000

    Allow watch directory to be set via CLI
    `--reload` always uses the working directory, added `--watch` so that
    the directory may be set to another value.

--- tests/test_rundramatiq_command.py
+++ tests/test_rundramatiq_command.py
@@ -63,7 +63,7 @@ def test_rundramatiq_can_run_dramatiq_reload(execvp_mock):
     # Given an output buffer
     buff = StringIO()
 
-    # When I call the rundramatiq command with --reload-use-polling
+    # When I call the rundramatiq command with --reload
     call_command("rundramatiq", "--reload", stdout=buff)
 
     # Then execvp should be called with the appropriate arguments
@@ -79,6 +79,38 @@ def test_rundramatiq_can_run_dramatiq_reload(execvp_mock):
         expected_exec_name, "--path", ".", "--processes", cores, "--threads", threads,
         "--worker-shutdown-timeout", "600000",
         "--watch", ".",
+        "django_dramatiq.setup",
+        "django_dramatiq.tasks",
+        "tests.testapp1.tasks",
+        "tests.testapp2.tasks",
+        "tests.testapp3.tasks.other_tasks",
+        "tests.testapp3.tasks.tasks",
+        "tests.testapp3.tasks.utils",
+        "tests.testapp3.tasks.utils.not_a_task",
+    ])
+
+
+@patch("os.execvp")
+def test_rundramatiq_can_run_dramatiq_watch(execvp_mock):
+    # Given an output buffer
+    buff = StringIO()
+
+    # When I call the rundramatiq command with --watch
+    call_command("rundramatiq", "--watch", "/path/to/watch/dir", stdout=buff)
+
+    # Then execvp should be called with the appropriate arguments
+    cores = str(rundramatiq.NPROCS)
+    threads = str(rundramatiq.NTHREADS)
+    expected_exec_name = "dramatiq"
+    expected_exec_path = os.path.join(
+        os.path.dirname(sys.executable),
+        expected_exec_name,
+    )
+
+    execvp_mock.assert_called_once_with(expected_exec_path, [
+        expected_exec_name, "--path", ".", "--processes", cores, "--threads", threads,
+        "--worker-shutdown-timeout", "600000",
+        "--watch", "/path/to/watch/dir",
         "django_dramatiq.setup",
         "django_dramatiq.tasks",
         "tests.testapp1.tasks",
