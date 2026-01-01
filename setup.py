3ce266f8011ab3a691cd478d0b94537266b4dbc8
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-01-02 02:04:43 +0300

    Add missing classifier entry for Django 5.2

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
