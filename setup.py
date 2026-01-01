f90bce45e35904587c8d2757acf6ac3c3128d828
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-01-02 02:09:17 +0300

    Add support for Django 6.0

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
         "Framework :: Django :: 5.2",
+        "Framework :: Django :: 6.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
